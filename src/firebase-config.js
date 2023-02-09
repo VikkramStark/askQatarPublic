29dc55bdbf8cadaef4d894bd4c01afc34cd494d0
VikkramStark <vikkram26stark@gmail.com>
2023-02-09 15:28:17 +0530

    Optimal Out

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,12 +8,12 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0-yXI6OEZv_xtjntEwyqTbVOZLQ6Y62w",
-  authDomain: "dev-auth-dbd81.firebaseapp.com",
-  projectId: "dev-auth-dbd81",
-  storageBucket: "dev-auth-dbd81.appspot.com",
-  messagingSenderId: "448241492847",
-  appId: "1:448241492847:web:55846ed28e2a427dd13e9a"
+  apiKey: process.env.REACT_APP_API_KEY, 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, 
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
